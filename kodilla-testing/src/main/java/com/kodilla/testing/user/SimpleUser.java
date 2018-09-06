b42a41e66b843b3a99a126ca47547ab1426fac9a
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-09-06 16:01:28 +0200

    Refactor SimpleUser.java and add tests

--- kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -1,13 +1,20 @@
 package com.kodilla.testing.user;
 
 public class SimpleUser {
-    private String username;
 
-    public SimpleUser(String username) {
+    private String username;
+    private String realName;
+
+    public SimpleUser(String username, String realName) {
         this.username = username;
+        this.realName = realName;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getRealName() {
+        return realName;
     }
 }
