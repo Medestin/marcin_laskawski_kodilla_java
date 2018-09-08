0489d3e2c4c36d7cf41473d69fb5613b162a5f9f
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-09-08 21:42:44 +0200

    feat: add Mockito and weather forecasts. Kodilla 6.5

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.testing.weather.mock;
+
+import com.kodilla.testing.weather.stub.Temperatures;
+import com.kodilla.testing.weather.stub.WeatherForecast;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class WeatherForecastTestSuite {
+    @Test
+    public void testCalculateForecastWithMock() {
+        //Given
+        Temperatures temperaturesMock = mock(Temperatures.class);
+        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
+        HashMap<Integer, Double> temperaturesMap = new HashMap<Integer, Double>();
+        temperaturesMap.put(0, 25.5);
+        temperaturesMap.put(1, 26.2);
+        temperaturesMap.put(2, 24.8);
+        temperaturesMap.put(3, 25.2);
+        temperaturesMap.put(4, 26.1);
+
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+
+        //When
+        int quantityOfSensors = weatherForecast.calculateForecast().size();
+
+        //Then
+        Assert.assertEquals(5, quantityOfSensors);
+    }
+}
