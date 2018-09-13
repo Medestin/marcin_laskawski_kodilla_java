c38264308484d41952314ba53354ecf6f685905e
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-09-13 16:44:38 +0200

    feat: Kodilla task 7.3 Streaming ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,20 +1,25 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.book.*;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
 
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
 
-        BookDirectory theBookDirectory = new BookDirectory();
-        String theResultStringOfBooks = theBookDirectory.getList().stream()
-                .filter(book -> book.getYearOfPublication() > 2005)
-                .map(Book::toString)
-                .collect(Collectors.joining(",\n","<<",">>"));
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> filteredUserMap = forum.getUserList().stream()
+                .filter(n -> n.getSex() == 'M')
+                .filter(n -> LocalDate.now().getYear() - n.getDateOfBirth().getYear() >= 20)
+                .filter(n -> n.getPostCount() > 0)
+                .collect(Collectors.toMap(ForumUser::getId, ForumUser -> ForumUser));
 
-        System.out.println(theResultStringOfBooks);
+        filteredUserMap.entrySet().stream()
+                .map(entry -> entry.getKey() + " " + entry.getValue())
+                .forEach(System.out::println);
 
     }
 }
