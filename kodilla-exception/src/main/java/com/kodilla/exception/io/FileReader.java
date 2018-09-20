6d80e36f2470757ca9b881b0d4b4bfe5bff82780
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-09-20 13:41:32 +0200

    feat: Kodilla 8.3 Throw/Throws

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -9,14 +9,14 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
 
         try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
             fileLines.forEach(System.out::println);
         } catch (IOException e) {
-            System.out.println("EXCEPTION! :" + e);
+            throw new FileReaderException();
         } finally {
             System.out.println("\n- After all this time?\n- Always.");
         }
--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -2,11 +2,18 @@ package com.kodilla.exception.main;
 
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
 
 public class ExceptionModuleRunner {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+
+
+        try {
+            fileReader.readFile();
+        } catch (FileReaderException e) {
+            System.out.println("Something went terribly wrong. We have to go back.");
+        }
 
     }
 }
