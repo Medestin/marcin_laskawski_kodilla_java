c783d7bfa093a374f34f1b1a57d78b28a6dda1eb
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-10-08 13:46:34 +0200

    Kodilla 10.3

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -1,6 +1,5 @@
 package com.kodilla.spring.library;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,11 +8,16 @@ import java.util.List;
 @Service
 public final class Library {
     private final List<String> books = new ArrayList<>();
-
-    @Autowired
     private LibraryDbController libraryDbController;
 
-     public void saveToDb() {
+    public Library(final LibraryDbController libraryDbController) {
+        this.libraryDbController = libraryDbController;
+    }
+
+    public Library() {
+    }
+
+    public void saveToDb() {
         libraryDbController.saveData();
     }
 
