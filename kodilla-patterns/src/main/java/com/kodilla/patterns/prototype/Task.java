2149635093bf12001a89ef76727d4dfdba66fb83
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-10-10 13:31:22 +0200

    feat: Kodilla 11.2 Prototype - Shallow & Deep copy

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/Task.java
@@ -0,0 +1,18 @@
+package com.kodilla.patterns.prototype;
+
+public final class Task {
+    final private String name;
+
+    public Task(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "      Task [" + name + "]";
+    }
+}
