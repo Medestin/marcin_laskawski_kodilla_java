f6e58028b6730f5d118d4eacc44666dd5847cb3e
Marcin Laskawski <laskawski.marcin@gmail.com>
2018-10-10 18:20:14 +0200

    Kodilla 11.3 Strategy

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/CustomerTestSuite.java
@@ -0,0 +1,45 @@
+package com.kodilla.patterns.strategy;
+
+import com.kodilla.patterns.strategy.predictors.AggressivePredictor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CustomerTestSuite {
+    @Test
+    public void testDefaultInvestingStrategies() {
+        //Given
+        Customer steven = new IndividualCustomer("Steven Links");
+        Customer john = new IndividualYoungCustomer("John Hemerald");
+        Customer kodilla = new CorporateCustomer("Kodilla");
+
+        //When
+        String stevenShouldBuy = steven.predict();
+        System.out.println("Steven should: " + stevenShouldBuy);
+        String johnShouldBuy = john.predict();
+        System.out.println("John should: " + johnShouldBuy);
+        String kodillaShouldBuy = kodilla.predict();
+        System.out.println("Kodilla should: " + kodillaShouldBuy);
+
+        //Then
+        assertEquals("[Conservative predictor] Buy debentures of XYZ", stevenShouldBuy);
+        assertEquals("[Aggressive predictor] Buy stock of XYZ", johnShouldBuy);
+        assertEquals("[Balanced predictor] Buy shared units of Fund XYZ", kodillaShouldBuy);
+    }
+
+    @Test
+    public void testIndividualInvestingStrategy() {
+        //Given
+        Customer steven = new IndividualCustomer("Steven Links");
+
+        //When
+        String stevenShouldBuy = steven.predict();
+        System.out.println("Steven should: " + stevenShouldBuy);
+        steven.setBuyingStrategy(new AggressivePredictor());
+        stevenShouldBuy = steven.predict();
+        System.out.println("Steven now should: " + stevenShouldBuy);
+
+        //Then
+        assertEquals("[Aggressive predictor] Buy stock of XYZ", stevenShouldBuy);
+    }
+}
