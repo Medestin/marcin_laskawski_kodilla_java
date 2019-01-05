352c3cca1fb2c1d84396009aa01df8f80692b65a
Marcin Laskawski <laskawski.marcin@gmail.com>
2019-01-05 16:45:16 +0100

    Kodilla 22.3 DB - procedures and functions

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.jdbc;
+
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+
+public class StoredProcTestSuite {
+
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"NOT SET\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        statement.execute(sqlProcedureCall);
+
+        String sqlCount = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL = \"NOT SET\"";
+        ResultSet rs = statement.executeQuery(sqlCount);
+        int counter = -1;
+        if(rs.next()){
+            counter = rs.getInt("HOW_MANY");
+        }
+        assertEquals(0, counter);
+    }
+}
